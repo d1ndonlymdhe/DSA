04cda8d695e85267be85173bc406765b439d19b7
d1ndonlymdhe <ayushshrestha8920@protonmail.com>
2024-01-29 20:39:30 +0545

    sorting

--- sort/exchangesort.cpp
+++ sort/exchangesort.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void sort(int *arr, int size) {
+void sort(int arr[], int size) {
   for (int i = 0; i < size - 1; i++) {
-    for (int j = 0; j < size - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        int t = arr[j+1];
-        arr[j+1] = arr[j];
+    for (int j = i + 1; j < size; j++) {
+      if (arr[i] > arr[j]) {
+        int t = arr[i];
+        arr[i] = arr[j];
         arr[j] = t;
       }
     }
@@ -14,7 +14,7 @@ void sort(int *arr, int size) {
 }
 
 int main() {
-  int list[] = {80, 30, 10, 20, 5, 3, 2, 1000, 100, 99, 90, -50};
+  int list[] = {70, 80, 30, 10, 20, 5, 3, 100, 99, 50};
   int arrSize = (sizeof list) / (sizeof(int));
   sort(list, arrSize);
   for (int i = 0; i < arrSize; i++) {
--- sort/quicksort.cpp
+++ sort/quicksort.cpp
@@ -1,76 +1,58 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-vector<int> merge(vector<int> left, int pivot, vector<int> right)
-{
-    vector<int> arr;
-    for (size_t i = 0; i < left.size(); i++)
-    {
-        arr.push_back(left[i]);
-    }
-    arr.push_back(pivot);
-    for (size_t i = 0; i < right.size(); i++)
-    {
-        arr.push_back(right[i]);
-    }
-    return arr;
+void swap(int *a, int *b) {
+  int t = *b;
+  *b = *a;
+  *a = t;
 }
 
-vector<int> sort(vector<int> arr)
-{
-    size_t size = arr.size();
-    if (size == 0)
-    {
-        return {};
+int findUp(vector<int> arr, int start, int end, int key) {
+  for (int i = start; i >= end; i--) {
+    if (arr[i] < key) {
+      return i;
     }
-    if (size == 1)
-    {
-        return arr;
-    }
-    if (size == 2)
-    {
-        if (arr[1] < arr[0])
-        {
-            int t = arr[0];
-            arr[0] = arr[1];
-            arr[1] = t;
-        }
-        return arr;
-    }
-    vector<int> left, right;
-    size_t pivotIdx = size / 2;
-    int smallEqCount = 0;
-    int largeCount = 0;
-    for (size_t i = 0; i < size; i++)
-    {
-        if (i != pivotIdx)
-        {
-
-            if (arr[i] <= arr[pivotIdx])
-            {
-                smallEqCount++;
-                left.push_back(arr[i]);
-            }
-            else
-            {
-                largeCount++;
-                right.push_back(arr[i]);
-            }
-        }
-    }
-    vector<int> leftSort = sort(left);
-    int pivot = arr[pivotIdx];
-    vector<int> rightSort = sort(right);
-    return merge(leftSort, pivot, rightSort);
+  }
+  return end;
 }
 
-int main()
-{
-    vector<int> arr = {1, 6, 4, 9, 3, 2, 0, 10, 8, 7, 89, 65, 98};
-    vector<int> b = sort(arr);
-    for (size_t i = 0; i < b.size(); i++)
-    {
-        cout << b[i] << endl;
+int findDown(vector<int> arr, int start, int end, int key) {
+  for (int i = start; i < end; i++) {
+    if (arr[i] > key) {
+      return i;
     }
+  }
+  return end;
+}
+
+void sort(vector<int> &arr, int start, int end) {
+  int size = end - start;
+  if (size > 1) {
+    int key = arr[start];
+    int up = end - 1;
+    int down = start;
+    while (down < up) {
+      if (up >= 0) {
+        up = findUp(arr, up, start, key);
+      }
+      if (down < end) {
+        down = findDown(arr, down, end, key);
+      }
+      if (up > down) {
+        swap(&arr[up], &arr[down]);
+      }
+    }
+    swap(&arr[start], &arr[up]);
+    sort(arr, 0, up);
+    sort(arr, down, end);
+  }
+  return;
+}
+
+int main() {
+  vector<int> arr = {24, 2, 45, 20, -67,56, 75, 2, 56, 99, 53, 12};
+  sort(arr, 0, arr.size());
+  for (int a : arr) {
+    cout << a << endl;
+  }
 }
